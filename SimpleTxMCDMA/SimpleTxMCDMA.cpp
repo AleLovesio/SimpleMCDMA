55b3d1fd0cf1001a190ceb09d948fa44af3ddbcc
AleLovesio <alessandrolovesio@gmail.com>
2023-04-27 02:19:50 +0200

    Tx MCDMA updated for byte alignement.

--- SimpleTxMCDMA/SimpleTxMCDMA.cpp
+++ SimpleTxMCDMA/SimpleTxMCDMA.cpp
@@ -2,15 +2,22 @@
 #include "SimpleTxMCDMA.h"
 #include <string.h>
 
+#define NO_DEBUG
+#ifndef NO_DEBUG
+#include <iostream>
+#endif
+
 void SimpleTxMCDMA(uint32_t* mem, dataStream_t& TX_stream, channel_descr_t channel_descr[N_CHANNELS])
 {
-#pragma HLS INTERFACE mode=m_axi port=mem offset=off depth=2048
+#pragma HLS INTERFACE mode=m_axi port=mem offset=off max_widen_bitwidth=32 max_read_burst_length=128 depth=2048
 #pragma HLS INTERFACE mode=axis port=TX_stream depth=2048
 #pragma HLS INTERFACE mode=s_axilite port=channel_descr bundle=s_axi_ctrl
 #pragma HLS INTERFACE mode=s_axilite port=return bundle=s_axi_ctrl
 #pragma HLS DISAGGREGATE variable=channel_descr
 
 	uint32_t buffer[2][128];
+	ap_uint<4> keep_table[5] = { 0, 8, 12, 14, 15 };
+	uint32_t data_mask[5] = { 0, 0xff000000, 0xffff0000, 0xffffff00, 0xffffffff	};
 
 	for(int i = 0; i < N_CHANNELS; i++)
 	{
@@ -25,21 +32,27 @@ void SimpleTxMCDMA(uint32_t* mem, dataStream_t& TX_stream, channel_descr_t chann
 				memcpy(buffer[mem_transfers & 0x1], ((uint8_t *)mem) + channel_descr[i].addr + (mem_transfers*512), 512);
 				for(len = 0; len < 128  && len_remaining > 0; len++, len_remaining-=4)
 				{
+#pragma HLS PIPELINE ii = 1 rewind
 					dataPkt_t dataPkt;
-					dataPkt.data = buffer[mem_transfers & 0x1][len];
+					uint32_t data;
+					data = buffer[mem_transfers & 0x1][len];
 					dataPkt.dest = i;
 
-					if(len_remaining <= 0)
+					if(len_remaining <= 4)
 					{
+						dataPkt.data = data & data_mask[len_remaining];
 						dataPkt.last = 1;
-						ap_uint<4> keep = (-len_remaining) & 0xf;
-						dataPkt.keep = keep.reverse();
+						dataPkt.keep = keep_table[len_remaining];
 					}
 					else
 					{
+						dataPkt.data = data;
 						dataPkt.last = 0;
 						dataPkt.keep = 0xf;
 					}
+#ifndef NO_DEBUG
+					std::cout << "channel " << i << "mem_transfers " << mem_transfers << " len " << len << " len_remaining " << len_remaining << " dataPkt.data " << dataPkt.data << " dataPkt.dest " << dataPkt.dest << " dataPkt.last " << dataPkt.last << " dataPkt.keep " << dataPkt.keep << std::endl;
+#endif
 					TX_stream.write(dataPkt);
 				}
 			}
--- SimpleTxMCDMA/test_bench_SimpleTxMCDMA.cpp
+++ SimpleTxMCDMA/test_bench_SimpleTxMCDMA.cpp
@@ -6,19 +6,26 @@ int main()
 	int retval = 0;
 	channel_descr_t channel_descr[N_CHANNELS];
 	uint32_t data[512*N_CHANNELS];
+	uint32_t exp_data[512*N_CHANNELS];
 	int cnt[N_CHANNELS] = {0};
 	dataStream_t dataStream;
 	dataPkt_t dataPkt;
+	uint32_t data_mask[5] = { 0, 0xff000000, 0xffff0000, 0xffffff00, 0xffffffff	};
 
 	std::cout << "Initializing test data." << std::endl;
-	for(int i = 0; i <512*N_CHANNELS; i++)
+	for(int i = 0; i <N_CHANNELS; i++)
 	{
-		data[i]=i;
+		for(int j = 0; j < 512; j++)
+		{
+			data[i*512 + j]=i*512 + j;
+			exp_data[i*512 + j]=i*512 + j;
+		}
 	}
 	for(int i = 0; i <N_CHANNELS; i++)
 	{
 		channel_descr[i].enable = true;
-		channel_descr[i].len=512*4;
+		channel_descr[i].len=511*4+i+1; //512*4;
+		exp_data[i*512+511] &= data_mask[i+1];
 		channel_descr[i].addr=(uint32_t)i*512*4;
 		cnt[i] = i*512;
 	}
@@ -27,23 +34,27 @@ int main()
 	SimpleTxMCDMA(data, dataStream, channel_descr);
 
 	std::cout << "Verifying functionality." << std::endl;
-	for(int i = 0; i <512*N_CHANNELS && retval == 0; i++)
+	for(int i = 0; i <N_CHANNELS && retval == 0; i++)
 	{
-		dataPkt = dataStream.read();
-		std::cout << "     Pkt #"<< i << ": Data: " << dataPkt.data << " Dest: " << dataPkt.dest << " Last: " << dataPkt.last << std::endl;
-		if(dataPkt.data == cnt[dataPkt.dest])
+		for(int j = 0; j < (channel_descr[i].len+3)/4; j++)
 		{
-			cnt[dataPkt.dest]++;
-		}
-		else
-		{
-			retval = -1;
-			std::cout << "Error on data." << std::endl;
-		}
-		if((dataPkt.last == 1 && dataPkt.data % 512 != 511) || (dataPkt.last == 0 && dataPkt.data % 512 == 511))
-		{
-			retval = -1;
-			std::cout << "Error on last." << std::endl;
+			dataPkt = dataStream.read();
+			std::cout << "     Pkt #"<< i << ","<< j << ": Data: " << dataPkt.data << " Dest: " << dataPkt.dest << " Last: " << dataPkt.last << " Keep: " << dataPkt.keep << std::endl;
+			if(dataPkt.data != exp_data[i*512 + j])
+			{
+				retval = -1;
+				std::cout << "Error on data. Expected: " << exp_data[i*512 + j] << std::endl;
+			}
+			if(((dataPkt.last == 1) && (j != ((channel_descr[i].len+3)/4)-1)) || ((dataPkt.last == 0) && (j == ((channel_descr[i].len+3)/4)-1)))
+			{
+				retval = -1;
+				std::cout << "Error on last." << std::endl;
+			}
+			if((dataPkt.keep != 0xf && (j+1)*4 < channel_descr[i].len)) /* TODO: Check cases where keep is != 0xf */
+			{
+				retval = -1;
+				std::cout << "Error on keep. " << dataPkt.keep << " " << j*4 << " " << channel_descr[i].len << std::endl;
+			}
 		}
 	}
 	if(retval == 0)
